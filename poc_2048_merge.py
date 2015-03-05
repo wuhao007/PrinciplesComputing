431a96e7cae0e6540aff5addfffa19596a6266d4
Hao Wu <hao.wu@HaowuSE-MBP.local>
2015-03-05 12:51:42 -0800

    some change?

--- poc_2048_merge.py
+++ poc_2048_merge.py
@@ -7,23 +7,23 @@ def merge(line):
     Function that merges a single row or column in 2048.
     """
     # replace with your code
-    lineLen = len(line)
-    mergeLine = line[:]
-    lineFlag = [False] * lineLen
+    line_len = len(line)
+    merge_line = line[:]
+    line_flag = [False] * line_len
 
-    for i in range(1, lineLen):
-        if mergeLine != 0:
-            k = i
-            for j in range(i - 1, -1, -1):
-                if mergeLine[j] == 0:
-                    k = j
+    for ptr_i in range(1, line_len):
+        if merge_line != 0:
+            ptr_k = ptr_i
+            for ptr_j in range(ptr_i - 1, -1, -1):
+                if merge_line[ptr_j] == 0:
+                    ptr_k = ptr_j
                 else:
                     break
-            mergeLine[k], mergeLine[i] = mergeLine[i], mergeLine[k]
-            if k > 0 and mergeLine[k] == mergeLine[k - 1] and lineFlag[k - 1] == False:
-                mergeLine[k - 1], mergeLine[k] = 2 * mergeLine[k - 1], 0 
-                lineFlag[k - 1] = True
-    return mergeLine
+            merge_line[ptr_k], merge_line[ptr_i] = merge_line[ptr_i], merge_line[ptr_k]
+            if ptr_k > 0 and merge_line[ptr_k] == merge_line[ptr_k - 1] and line_flag[ptr_k - 1] == False:
+                merge_line[ptr_k - 1], merge_line[ptr_k] = 2 * merge_line[ptr_k - 1], 0 
+                line_flag[ptr_k - 1] = True
+    return merge_line
 
 print merge([2, 0, 2, 4])
 print merge([0, 0, 2, 2])
